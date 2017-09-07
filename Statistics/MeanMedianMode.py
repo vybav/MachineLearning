3247f25528edc33ee0116a7f8aedc6299b6fb42a
Vaibhav Kumar <vaibhavkumar@vaibhavs-macbook-pro.local>
2017-09-07 01:06:52 -0500

    Mode Implemented

--- Statistics/MeanMedianMode.py
+++ Statistics/MeanMedianMode.py
@@ -1,8 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats as stat
 
 incomes = np.random.normal(27000, 15000, 10000)
-print(np.mean(incomes))
+print("Mean:", np.mean(incomes))
+print("Median:",  np.median(incomes))
 
 plt.hist(incomes, 50)
 plt.show()
+
+# Mode
+ages = np.random.randint(17, high=90, size=500)
+print(ages)
+
+print(stat.mode(ages))
